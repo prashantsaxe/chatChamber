5ad10129b38270db3800767382d97605e42d4650
Prashant <prashantiiitnag@gmail.com>
2025-01-30 02:37:46 +0530

    d3

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,5 @@
-export const HOST =  import.meta.env.VITE_SERVER_URL;
+export const HOST = import.meta.env.VITE_SERVER_URL || "http://localhost:5000";
+
 
 export const AUTH_ROUTE = "api/auth";
 export const SIGNUP_ROUTE = `${AUTH_ROUTE}/signup`;
